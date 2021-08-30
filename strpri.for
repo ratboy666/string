fb5cf2a3948e2f12ef0b7cdb4332e708221dcc4d
Fred Weigel <fred_weigel@hotmail.com>
2021-08-30 12:43:01 -0400

    Fix empty string print in STRPRI
    Empty string (length 0) would not print properly in STRPRI routine. FORTRAN IV DO loops always execute at least once. Fixed.

--- strpri.for
+++ strpri.for
@@ -18,8 +18,12 @@ C
       BYTE BUF(132)
 C
       J = STRLEN(S$)
+      IF (J .EQ. 0) GO TO 1
       CALL STRGET(S$,BUF,132)
       WRITE (1,300) (BUF(I),I=1,J)
   300 FORMAT(1X,132A1)
+      RETURN
+    1 WRITE (1,301)
+  301 FORMAT(1X)
       RETURN
       END
